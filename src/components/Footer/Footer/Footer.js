d25627fdf50894cbfab250a5700fba8249cc455b
Ataur-rahman-01 <iamataur0911@gmail.com>
2021-04-20 23:00:58 +0600

    gg

--- src/components/Footer/Footer/Footer.js
+++ src/components/Footer/Footer/Footer.js
@@ -9,68 +9,62 @@ import {
 
 const Footer = () => {
   return (
-    <div classnameName="row m-5">
-      <div classnameName="col-md-4">
+    <div className="row m-5">
+      <div className="col-md-4">
         <div>
-          <ul classname="list-group list-group-flush">
-            <li classname="list-group-item">
-              <FontAwesomeIcon
-                classnameName="text-info me-3"
-                icon={faHouseUser}
-              />
+          <ul className="list-group list-group-flush">
+            <li className="list-group-item">
+              <FontAwesomeIcon className="text-info me-3" icon={faHouseUser} />
               Detheme Design Studio
             </li>
-            <li classname="list-group-item">
+            <li className="list-group-item">
               <FontAwesomeIcon
-                classnameName="text-info me-3"
+                className="text-info me-3"
                 icon={faLocationArrow}
               />
               1428 Callison Laney Buoy Building
               <br />
               201/VRY Virginia, VA 22902
             </li>
-            <li classname="list-group-item">
-              <FontAwesomeIcon classnameName="text-info me-3" icon={faPhone} />
+            <li className="list-group-item">
+              <FontAwesomeIcon className="text-info me-3" icon={faPhone} />
               +62 9282739-44-539
             </li>
-            <li classname="list-group-item">
-              <FontAwesomeIcon
-                classnameName="text-info me-3"
-                icon={faEnvelope}
-              />
+            <li className="list-group-item">
+              <FontAwesomeIcon className="text-info me-3" icon={faEnvelope} />
               contact@mywebsite.com
             </li>
           </ul>
         </div>
       </div>
-      <div classnameName="col-md-4">
+      <div className="col-md-4">
         <div>
-          <ul classname="list-group list-group-flush">
+          <ul className="list-group list-group-flush">
             <h5>THE COMPANY</h5>
-            <li classname="list-group-item">Career</li>
-            <li classname="list-group-item">Angel Investor</li>
-            <li classname="list-group-item">Terms of Service</li>
-            <li classname="list-group-item">Legal Information</li>
-            <li classname="list-group-item">Credits</li>
+            <li className="list-group-item">Career</li>
+            <li className="list-group-item">Angel Investor</li>
+            <li className="list-group-item">Terms of Service</li>
+            <li className="list-group-item">Legal Information</li>
+            <li className="list-group-item">Credits</li>
           </ul>
         </div>
       </div>
-      <div classnameName="col-md-2">
-        <ol classname="list-group list-group-numbered">
+      <div className="col-md-2">
+        <ol className="list-group list-group-numbered">
           <h5>USEFUL LINKS</h5>
-          <li classname="list-group-item">Map direction</li>
-          <li classname="list-group-item">Live events</li>
-          <li classname="list-group-item">Get in touch</li>
-          <li classname="list-group-item">Knowledgebase</li>
+          <li className="list-group-item">Map direction</li>
+          <li className="list-group-item">Live events</li>
+          <li className="list-group-item">Get in touch</li>
+          <li className="list-group-item">Knowledgebase</li>
         </ol>
       </div>
-      <div classnameName="col-md-2">
-        <ol classname="list-group list-group-numbered">
+      <div className="col-md-2">
+        <ol className="list-group list-group-numbered">
           <h5>FOLLOW US</h5>
-          <li classname="list-group-item">Facebook</li>
-          <li classname="list-group-item">Twitter</li>
-          <li classname="list-group-item">Instagram</li>
-          <li classname="list-group-item">LinkedIn</li>
+          <li className="list-group-item">Facebook</li>
+          <li className="list-group-item">Twitter</li>
+          <li className="list-group-item">Instagram</li>
+          <li className="list-group-item">LinkedIn</li>
         </ol>
       </div>
     </div>
